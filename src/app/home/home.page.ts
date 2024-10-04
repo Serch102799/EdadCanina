51491a25e2da3838c014c2734746d6e754d525b2
Serch102799 <14565@utcv.edu.mx>
2024-10-03 20:51:41 -0600

    Se agrego Imagen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {IonButton, IonImg, IonText, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonImg, IonText, IonInput, FormsModule],
 })
 export class HomePage {
+
+edad:number = 0;
+resultado:string = '';
+
   constructor() {}
+
+  calcular() {
+    let edadCanina: number= this.edad * 7;
+    this.resultado = 'La edad Canina de su mascota es: '+ edadCanina + ' años';
+
+  }
+
+
+
 }
