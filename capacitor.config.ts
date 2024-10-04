674ed99dd7eb0cc27e22daf5c75631c11eab1137
Serch102799 <14565@utcv.edu.mx>
2024-10-03 20:38:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edadCanina',
+  webDir: 'www'
+};
+
+export default config;
